1fa87ed1326881876a4d499c8d2a8b96f1f96212
kan25d5 <43536347+kan25d5@users.noreply.github.com>
2020-06-09 18:00:18 +0900

    add music system (command only)
    music systemを追加（コマンド操作のみ）

--- bot/ayano.py
+++ bot/ayano.py
@@ -11,39 +11,11 @@ class Ayano(commands.Bot):
     def __add_cogs(self):
         """dialog_systemsが持つコグを綾乃ちゃんに登録する"""
         for system in self.dialog_systems:
-            if system.cogs is None:
-                continue
-            for cog in system.cogs:
-                self.add_cog(cog(self))
+            if system.cog:
+                self.add_cog(system.cog(self, system))
 
     async def on_ready(self):
         print("-----")
         print(self.user.name)
         print(self.user.id)
         print("-----")
-
-    async def on_message(self, message: discord.Message):
-        print("input")
-        print(message.content)
-
-        if message.author.bot:
-            print("utterance myself")
-            return
-        if self.user.id in message.mentions:
-            print("not mention")
-            return
-
-        channel = message.channel
-        text = message.content
-        out = None
-
-        # 対話システムの応答
-        for system in self.dialog_systems:
-            out = system.reply(text)
-            if out:
-                out = message.author.mention + " " + out
-                await channel.send(out)
-                print("output")
-                print(out)
-                print("-------")
-                return
